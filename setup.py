15bc0432f0a7cee68a37b56872288ecef019bc18
Jens Vagelpohl <jens@plyp.com>
2023-10-05 14:28:34 +0200

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.hookable',
-      version='6.0',
+      version='6.1.dev0',
       url='http://github.com/zopefoundation/zope.hookable',
       license='ZPL 2.1',
       description='Zope hookable',
