c23843932f3389f3ccef04b0597bee54a00e0cee
Tres Seaver <tseaver@palladion.com>
2024-05-24 17:49:36 -0400

    refactor: apply Mozilla style w/ 4 char indents
    Using https://formatter.org/c-formatter.

--- src/zope/hookable/_zope_hookable.c
+++ src/zope/hookable/_zope_hookable.c
@@ -21,118 +21,117 @@
 #include "Python.h"
 #include "structmember.h"
 
-typedef struct {
+typedef struct
+{
     PyObject_HEAD
-    PyObject *old;
-    PyObject *implementation;
+    PyObject* old;
+    PyObject* implementation;
 } hookable;
 
 static int
-hookable_init(hookable *self, PyObject *args, PyObject *kwds)
+hookable_init(hookable* self, PyObject* args, PyObject* kwds)
 {
-  static char *kwlist[] = {"implementation", NULL};
-  PyObject *implementation;
+    static char* kwlist[] = { "implementation", NULL };
+    PyObject* implementation;
 
-  if (! PyArg_ParseTupleAndKeywords(args, kwds, "O:hookable", kwlist,
-                                    &implementation))
-    return -1;
+    if (!PyArg_ParseTupleAndKeywords(
+          args, kwds, "O:hookable", kwlist, &implementation))
+        return -1;
 
-  Py_INCREF(implementation);
-  Py_INCREF(implementation);
-  Py_XDECREF(self->old);
-  self->old = implementation;
-  Py_XDECREF(self->implementation);
-  self->implementation = implementation;
+    Py_INCREF(implementation);
+    Py_INCREF(implementation);
+    Py_XDECREF(self->old);
+    self->old = implementation;
+    Py_XDECREF(self->implementation);
+    self->implementation = implementation;
 
-  return 0;
+    return 0;
 }
 
 static int
-hookable_traverse(hookable *self, visitproc visit, void *arg)
+hookable_traverse(hookable* self, visitproc visit, void* arg)
 {
-  if (self->implementation != NULL && visit(self->implementation, arg) < 0)
-    return -1;
-  if (self->old != NULL
-      && self->old != self->implementation
-      && visit(self->old, arg) < 0
-      )
-    return -1;
+    if (self->implementation != NULL && visit(self->implementation, arg) < 0)
+        return -1;
+    if (self->old != NULL && self->old != self->implementation &&
+        visit(self->old, arg) < 0)
+        return -1;
 
-  return 0;
+    return 0;
 }
 
 static int
-hookable_clear(hookable *self)
+hookable_clear(hookable* self)
 {
-  Py_XDECREF(self->old);
-  self->old = NULL;
-  Py_XDECREF(self->implementation);
-  self->implementation = NULL;
-  return 0;
+    Py_XDECREF(self->old);
+    self->old = NULL;
+    Py_XDECREF(self->implementation);
+    self->implementation = NULL;
+    return 0;
 }
-
 
 static void
-hookable_dealloc(hookable *self)
+hookable_dealloc(hookable* self)
 {
-  PyObject_GC_UnTrack((PyObject *)self);
-  Py_XDECREF(self->old);
-  Py_XDECREF(self->implementation);
-  Py_TYPE(self)->tp_free((PyObject*)self);
+    PyObject_GC_UnTrack((PyObject*)self);
+    Py_XDECREF(self->old);
+    Py_XDECREF(self->implementation);
+    Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
-static PyObject *
-hookable_sethook(hookable *self, PyObject *implementation)
+static PyObject*
+hookable_sethook(hookable* self, PyObject* implementation)
 {
-  PyObject *old;
+    PyObject* old;
 
-  old = self->implementation;
-  Py_INCREF(implementation);
-  self->implementation = implementation;
+    old = self->implementation;
+    Py_INCREF(implementation);
+    self->implementation = implementation;
 
-  if (old == NULL)
-    {
-      Py_INCREF(Py_None);
-      return Py_None;
+    if (old == NULL) {
+        Py_INCREF(Py_None);
+        return Py_None;
     }
 
-  return old;
+    return old;
 }
 
-static PyObject *
-hookable_reset(hookable *self)
+static PyObject*
+hookable_reset(hookable* self)
 {
-  Py_XINCREF(self->old);
-  Py_XDECREF(self->implementation);
-  self->implementation = self->old;
-  Py_INCREF(Py_None);
-  return Py_None;
+    Py_XINCREF(self->old);
+    Py_XDECREF(self->implementation);
+    self->implementation = self->old;
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 static struct PyMethodDef hookable_methods[] = {
-  {"sethook",   (PyCFunction)hookable_sethook, METH_O,
-   "Set the hook implementation for the hookable object"},
-  {"reset", (PyCFunction)hookable_reset, METH_NOARGS,
-   "Reset the hook to the original value"},
-  {NULL,        NULL}       /* sentinel */
+    { "sethook",
+      (PyCFunction)hookable_sethook,
+      METH_O,
+      "Set the hook implementation for the hookable object" },
+    { "reset",
+      (PyCFunction)hookable_reset,
+      METH_NOARGS,
+      "Reset the hook to the original value" },
+    { NULL, NULL } /* sentinel */
 };
 
-
-static PyObject *
-hookable_call(hookable *self, PyObject *args, PyObject *kw)
+static PyObject*
+hookable_call(hookable* self, PyObject* args, PyObject* kw)
 {
-  if (self->implementation != NULL)
-    return PyObject_Call(self->implementation, args, kw);
-  PyErr_SetString(PyExc_TypeError, "Hookable has no implementation");
-  return NULL;
+    if (self->implementation != NULL)
+        return PyObject_Call(self->implementation, args, kw);
+    PyErr_SetString(PyExc_TypeError, "Hookable has no implementation");
+    return NULL;
 }
 
-
-static PyObject *
-hookable_getattro(hookable *self, PyObject *name)
+static PyObject*
+hookable_getattro(hookable* self, PyObject* name)
 {
-    PyObject *result = NULL;
-    const char *name_as_string;
+    PyObject* result = NULL;
+    const char* name_as_string;
     int maybe_special_name;
 
     name_as_string = PyUnicode_AsUTF8(name);
@@ -171,20 +170,21 @@ hookable_getattro(hookable *self, PyObject *name)
 }
 
 static PyMemberDef hookable_members[] = {
-  { "original", T_OBJECT_EX, offsetof(hookable, old), READONLY },
-  { "implementation", T_OBJECT_EX, offsetof(hookable, implementation), READONLY },
-  {NULL}    /* Sentinel */
+    { "original", T_OBJECT_EX, offsetof(hookable, old), READONLY },
+    { "implementation",
+      T_OBJECT_EX,
+      offsetof(hookable, implementation),
+      READONLY },
+    { NULL } /* Sentinel */
 };
 
-
 static char Hookabletype__doc__[] =
-"Callable objects that support being overridden"
-;
+  "Callable objects that support being overridden";
 
 static PyTypeObject hookabletype = {
     PyVarObject_HEAD_INIT(NULL, 0)
     /* tp_name           */ "zope.hookable."
-                                "hookable",
+                            "hookable",
     /* tp_basicsize      */ sizeof(hookable),
     /* tp_itemsize       */ 0,
     /* tp_dealloc        */ (destructor)&hookable_dealloc,
@@ -202,9 +202,9 @@ static PyTypeObject hookabletype = {
     /* tp_getattro       */ (getattrofunc)hookable_getattro,
     /* tp_setattro       */ (setattrofunc)0,
     /* tp_as_buffer      */ 0,
-    /* tp_flags          */ Py_TPFLAGS_DEFAULT
-                                | Py_TPFLAGS_BASETYPE
-                                | Py_TPFLAGS_HAVE_GC,
+    /* tp_flags          */ Py_TPFLAGS_DEFAULT |
+                            Py_TPFLAGS_BASETYPE |
+                            Py_TPFLAGS_HAVE_GC,
     /* tp_doc            */ Hookabletype__doc__,
     /* tp_traverse       */ (traverseproc)hookable_traverse,
     /* tp_clear          */ (inquiry)hookable_clear,
@@ -223,42 +223,42 @@ static PyTypeObject hookabletype = {
     /* tp_init           */ (initproc)hookable_init,
     /* tp_alloc          */ (allocfunc)0,
     /* tp_new            */ (newfunc)0 /*PyType_GenericNew*/,
-    /* tp_free           */ 0/*PyObject_GC_Del*/,
+    /* tp_free           */ 0 /*PyObject_GC_Del*/,
 };
 
-
-  #define MOD_ERROR_VAL NULL
-  #define MOD_SUCCESS_VAL(val) val
-  #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
-  #define MOD_DEF(ob, name, doc, methods) \
-      static struct PyModuleDef moduledef = { \
-        PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
-      ob = PyModule_Create(&moduledef);
+#define MOD_ERROR_VAL NULL
+#define MOD_SUCCESS_VAL(val) val
+#define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+#define MOD_DEF(ob, name, doc, methods)                                        \
+    static struct PyModuleDef moduledef = {                                    \
+        PyModuleDef_HEAD_INIT, name, doc, -1, methods,                         \
+    };                                                                         \
+    ob = PyModule_Create(&moduledef);
 
 static struct PyMethodDef module_methods[] = {
-    {NULL, NULL}
+    { NULL, NULL }  /* sentinel */
 };
 
 MOD_INIT(_zope_hookable)
 {
-  PyObject *m;
+    PyObject* m;
 
-  MOD_DEF(m, "_zope_hookable",
-    "Provide an efficient implementation for hookable objects",
-    module_methods)
+    MOD_DEF(m,
+            "_zope_hookable",
+            "Provide an efficient implementation for hookable objects",
+            module_methods)
 
-  if (m == NULL)
-    return MOD_ERROR_VAL;
+    if (m == NULL)
+        return MOD_ERROR_VAL;
 
-  hookabletype.tp_new = PyType_GenericNew;
-  hookabletype.tp_free = PyObject_GC_Del;
+    hookabletype.tp_new = PyType_GenericNew;
+    hookabletype.tp_free = PyObject_GC_Del;
 
-  if (PyType_Ready(&hookabletype) < 0)
-    return MOD_ERROR_VAL;
+    if (PyType_Ready(&hookabletype) < 0)
+        return MOD_ERROR_VAL;
 
-  if (PyModule_AddObject(m, "hookable", (PyObject *)&hookabletype) < 0)
-    return MOD_ERROR_VAL;
+    if (PyModule_AddObject(m, "hookable", (PyObject*)&hookabletype) < 0)
+        return MOD_ERROR_VAL;
 
-  return MOD_SUCCESS_VAL(m);
-
+    return MOD_SUCCESS_VAL(m);
 }
